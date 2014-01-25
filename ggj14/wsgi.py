685e15e789de53546e1c2eb0c5563c4299c988bf
Iain Dawson <iain@bldm.us>
2014-01-25 21:14:36 +0000

    Deployment-related changes.

--- ggj14/wsgi.py
+++ ggj14/wsgi.py
@@ -8,7 +8,10 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ggj14.settings")
+sys.path.append(os.path.realpath(os.path.join(__file__, '..', '..')))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
